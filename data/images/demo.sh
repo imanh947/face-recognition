4abdc5723e2e2b709797f66cbf86bb3b07ecfaa3
imanh947 <imanh947@gmail.com>
2020-08-26 10:12:58 +0800

    first commit

---
+++ data/images/demo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Set python path variable to the facenet src directory
+export PYTHONPATH=~/Projects/facenet/src
+
+# remove existing training alignment
+rm -rf ~/Projects/facenet/data/images/train_aligned
+
+# align training images
+python ~/Projects/facenet/src/align/align_dataset_mtcnn.py \
+~/Projects/facenet/data/images/train_raw \
+~/Projects/facenet/data/images/train_aligned \
+--image_size 160
+
+# remove existing testing alignment
+rm -rf ~/Projects/facenet/data/images/test_aligned
+
+# align testing images
+python ~/Projects/facenet/src/align/align_dataset_mtcnn.py \
+~/Projects/facenet/data/images/test_raw \
+~/Projects/facenet/data/images/test_aligned \
+--image_size 160
+
+# Train new classifier based on training images (generates pickle)
+python ~/Projects/facenet/src/classifier.py TRAIN \
+~/Projects/facenet/data/images/train_aligned/ \
+~/Projects/facenet/models/20180402-114759.pb \
+~/Projects/facenet/models/my_classifier.pkl
+
+# Test face matches using pickle file to classify matching images
+python ~/Projects/facenet/src/classifier.py CLASSIFY \
+~/Projects/facenet/data/images/test_aligned/ \
+~/Projects/facenet/models/20180402-114759.pb \
+~/Projects/facenet/models/my_classifier.pkl
